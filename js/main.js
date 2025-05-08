deda6bd4dc38a1d07dd856c4bb766f6da650352c
Zzhiarrifqii <zhiarifff@gmail.com>
2025-05-08 18:19:22 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('thank you').split('')
+    const titles = ('Happy 16th Birthday Miftaa:D').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
